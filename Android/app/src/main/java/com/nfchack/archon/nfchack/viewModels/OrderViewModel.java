13692138345125739251126ce39b2bb63994965b
Evan Holtrop <evan@inflowinventory.com>
2015-12-21 16:23:55 -0500

    Add OrderViewModel

---
+++ Android/app/src/main/java/com/nfchack/archon/nfchack/viewModels/OrderViewModel.java
@@ -0,0 +1,13 @@
+package com.nfchack.archon.nfchack.viewModels;
+
+import java.util.List;
+
+import reactiveUI.ReactiveList;
+import rx.subjects.BehaviorSubject;
+
+/**
+ * Created by Evan on 12/21/2015.
+ */
+public class OrderViewModel {
+    private ReactiveList<String> mProducts;
+}
